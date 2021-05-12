0cda1ef927691e115d9fc09fdbeedd1d267f679f
bbt <dinhngoctu.enci@gmail.com>
2021-05-12 14:56:10 +0700

    update ngx editor

--- ngzorro/wnote/src/app/app.component.ts
+++ ngzorro/wnote/src/app/app.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
-// import {Editor} from 'ngx-editor';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import EditorJS from '@editorjs/editorjs';
+import Header from '@editorjs/header';
+import {Editor} from 'ngx-editor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
+  ngOnDestroy(): void {
+  }
+  ngOnInit(): void {
+  }
   isCollapsed = true;
   isCollapsed2 = true;
   isCollapsedCate = false;
   isReverseArrow = false;
-  // editor = new Editor();
+  editor = new Editor();
+  // editor = new EditorJS({
+  //   holderId: 'editorjs',
+  //   tools: {
+  //     header: {
+  //       class: Header,
+  //       inlineToolbar: true
+  //     }
+  //   }
+  // });
+
 }
